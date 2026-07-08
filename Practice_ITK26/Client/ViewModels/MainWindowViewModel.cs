5bacc5285473319c2209b1af63085b47575c05b6
Nick <nikolajmnoan2@gmail.ru>
2026-07-08 14:57:33 +0300

    init(Practice_ITK26): создание проекта

---
+++ Practice_ITK26/Client/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,7 @@
+﻿namespace Client.ViewModels
+{
+    public class MainWindowViewModel : ViewModelBase
+    {
+        public string Greeting { get; } = "Welcome to Avalonia!";
+    }
+}
---
+++ Practice_ITK26/Client/ViewModels/ViewModelBase.cs
@@ -0,0 +1,8 @@
+﻿using ReactiveUI;
+
+namespace Client.ViewModels
+{
+    public abstract class ViewModelBase : ReactiveObject
+    {
+    }
+}

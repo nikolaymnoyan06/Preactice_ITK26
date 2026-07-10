3b0238f7ca335b0f76ceb6ec9238dc78fcacddc3
cmer-2 <ps309346@yandex.ru>
2026-07-10 15:36:21 +0300

    добавил комментарии в грфе клиента

--- Practice_ITK26/Client/Models/Dtos/NodeDto.cs
+++ Practice_ITK26/Client/Models/Dtos/NodeDto.cs
@@ -3,15 +3,28 @@ using System;
 using System.Collections.Generic;
 using System.Text;
 using System.Text.Json.Serialization;
+
 namespace Client.Dtos
 {
+    /// <summary>
+    /// Data Transfer Object (DTO) для представления узла графа.
+    /// Используется для передачи данных о вершине и её типе.
+    /// </summary>
     public class NodeDto
     {
+        // Уникальный идентификатор узла.
         public int Id { get; set; }
+
+        // Значение, хранящееся в узле (может быть любого типа).
         public object Value { get; set; }
+
+        // Тип узла (например, начальный, конечный, промежуточный и т.п.).
         [JsonConverter(typeof(JsonStringEnumConverter))]
         public NodeType Type { get; set; }
 
+        /// <summary>
+        /// Возвращает строковое представление узла в формате: "Значение (Тип)".
+        /// </summary>
         public override string ToString() => $"{Value} ({Type})";
     }
 }

18efce974f427985646b35642e738fbaa757bb85
PedroScob <peter.play9@gmail.com>
2026-07-16 00:13:48 +0300

    Добавлена перерисовка графа при переходе во вкладку визаулизация

--- Practice_ITK26/Client/Views/MainWindow.axaml.cs
+++ Practice_ITK26/Client/Views/MainWindow.axaml.cs
@@ -1,4 +1,5 @@
 using Avalonia.Controls;
+using Client.ViewModels;
 
 namespace Client.Views
 {
@@ -7,6 +8,30 @@ namespace Client.Views
         public MainWindow()
         {
             InitializeComponent();
+
+            // Находим вкладку по имени и подписываемся на событие
+            var tabControl = this.FindControl<TabControl>("MainTabControl");
+            if (tabControl != null)
+            {
+                tabControl.SelectionChanged += OnTabSelectionChanged;
+            }
+        }
+
+        private void OnTabSelectionChanged(object? sender, SelectionChangedEventArgs e)
+        {
+            // Проверяем, переключились ли на вкладку "Визуализация"
+            if (e.AddedItems.Count > 0 && e.AddedItems[0] is TabItem selectedTab)
+            {
+                var header = selectedTab.Header?.ToString();
+                if (header == "??? Визуализация" || header?.Contains("Визуализация") == true)
+                {
+                    // Получаем ViewModel и вызываем команду обновления узлов
+                    if (DataContext is MainWindowViewModel viewModel)
+                    {
+                        viewModel.LoadNodesCommand?.Execute(null);
+                    }
+                }
+            }
         }
     }
 }
